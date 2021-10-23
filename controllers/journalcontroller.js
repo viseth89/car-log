f468feb90719090b65de65c98b28dbda7434e368
viseth89 <viseth89@gmail.com>
2021-10-23 18:17:34 -0500

    Added Journal Post

--- controllers/journalcontroller.js
+++ controllers/journalcontroller.js
@@ -1,6 +1,7 @@
 const Express = require('express')
 const router = Express.Router();
-let validateJWT = require("../middleware/validate-jwt")
+let validateJWT = require("../middleware/validate-jwt");
+const { JournalModel } = require('../models');
 
 router.get('/practice', validateJWT, (req,res) => {
     res.send('this is a practice route')
@@ -9,5 +10,32 @@ router.get('/practice', validateJWT, (req,res) => {
 router.get('/about', (req,res) => {
     res.send('this is a about route')
 })
+
+
+
+
+/* 
+Journal Create */
+
+router.post("/create", validateJWT, async (req, res) => {
+    const { title, date, entry } = req.body.journal
+    const { id } = req.user;
+
+    const journalEntry = {
+        title,
+        date,
+        entry,
+        owner: id
+    }
+    try {
+        const newJournal = await JournalModel.create(journalEntry);
+        res.status(200).json(newJournal);
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+    JournalModel.create(journalEntry)
+})
+
+
 
 module.exports = router;
