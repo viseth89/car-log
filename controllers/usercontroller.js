11b9d35b6dabc1f8fa4e6de207376cee50abc1e8
viseth89 <viseth89@gmail.com>
2021-10-23 08:47:18 -0500

    Adding dynamic data to db and reviewing express.json

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -3,9 +3,11 @@ const { UserModel } = require("../models");
 
 router.post("/register", async(req,res) => {
 
+    let { email, password } = req.body.user;
+
     UserModel.create({
-        email: "user@email.com",
-        password: "password1234"
+        email,
+        password
     })
 
 })
